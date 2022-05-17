f293a35e58f330fe2aaee65b3baf19d78fabbf54
Usuario <Usuario@CPD02-M09>
2022-05-17 12:24:12 -0300

    Adicionar arquivos de projeto.

---
+++ AppListaSupermercado/AppListaSupermercado/View/FormularioCadastro.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppListaSupermercado.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FormularioCadastro : ContentPage
+    {
+        public FormularioCadastro()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new View.ListaProdutos());
+        }
+    }
+}
